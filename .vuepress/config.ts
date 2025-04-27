de99de62719d1abd1e9975042767246845aebc53
Kelly Adams <kelly@intellitect.com>
2025-04-27 14:32:29 -0700

    more config

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,12 +1,22 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
+import {viteBundler} from '@vuepress/bundler-vite'
+import {defaultTheme} from '@vuepress/theme-default'
+import {defineUserConfig} from 'vuepress'
 
 export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: defaultTheme(),
-  base: "/",
-  lang: 'en-US',
-  title: 'Spokane Free Store',
-  description: 'The website of the Spokane Free Store',
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        // default theme config
+        hostname: 'https://spokanefreestore.org',
+        navbar: [
+            {
+                text: 'Home',
+                link: '/',
+            },
+        ],
+    }),
+    base: "/",
+    lang: 'en-US',
+    title: 'Spokane Free Store',
+    description: 'The website of the Spokane Free Store',
+
 })
